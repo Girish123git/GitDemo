7ec76f2a82504b8bc9dcd19c1fa00e60c444f81f
girish khandekar <girishkhandekar07@gmail.com>
2024-03-07 03:05:50 +0530

    Studentlist

---
+++ Gitwork/src/studentlist/Student.java
@@ -0,0 +1,61 @@
+package studentlist;
+
+import java.util.ArrayList;
+
+public class Student {
+
+	public static void main(String[] args) {
+		
+	ArrayList<String> student =new ArrayList<String>();	
+		
+	student.add("Ram");
+	student.add("shyam");
+	System.out.println(student);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
